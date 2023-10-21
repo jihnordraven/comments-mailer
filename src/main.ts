30717e015eab89755b4136658d28a26d13a7edfb
flyingMerch123 <=>
2023-10-21 14:06:40 +0300

    setup: application setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { MicroserviceOptions, NestMicroservice, Transport } from '@nestjs/microservices'
+import { INestMicroservice, Logger } from '@nestjs/common'
+import { blue, red } from 'colorette'
+
+const logger: Logger = new Logger('bootstrap')
+
+const RMQ_HOST: string = String(process.env.RMQ_HOST)
+const RMQ_QUEUE: string = String(process.env.RMQ_QUEUE)
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app: INestMicroservice =
+		await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+			transport: Transport.RMQ,
+			options: {
+				urls: [RMQ_HOST],
+				queue: RMQ_QUEUE
+			}
+		})
+
+	await app
+		.listen()
+		.then(() => logger.log(blue('Microservice is listening')))
+		.catch((err: string) =>
+			logger.error(red(`Something went wrong... Learn more at: ${err}`))
+		)
 }
-bootstrap();
+bootstrap()
